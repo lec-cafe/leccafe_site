f16011cc200b2df6dcd0e0d4fe630583ad9e50d0
中村駿輝 <shnklife@gmail.com>
2020-03-09 12:50:46 +0900

    コンポーネント分割

---
+++ components/TopAccess.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="p-top__access">
+        <h1 class="p-top__titles c-textRed">Access</h1>
+        <img width="335px" height="235px" src="~/assets/images/top_img1.jpg" alt="">
+        <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/TopJoin.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="p-top__join">
+        <h1>Join</h1>
+        <div class="p-top_join__box">
+            <h3>詳しく知りたい方は<br>まずは資料請求！</h3>
+            <button>資料請求</button>
+            <button>申し込み</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .p-top__join {
+        background-color: #E15F52;
+        text-align: center;
+    }
+    .p-top_join__box {
+        border: solid 3px #ffffff;
+        border-radius: 10px;
+    }
+</style>
