25d1ad5d6a5f0655193476671312e4e6d942d2ff
中村駿輝 <shnklife@gmail.com>
2020-03-11 16:57:00 +0900

    -TopMeritを修正

--- pages/-TopMerit.vue
+++ pages/-TopMerit.vue
@@ -13,7 +13,7 @@
             </div>
 
             <div class="p-top__meritContentsSectionLeft">
-                <img class="p-top__meritContentsSectionLeftImage" src="~/assets/images/p-merit_img02@2x.png" alt="スクールイラスト">
+                <img class="p-top__meritContentsSectionLeftImage" src="~/assets/images/p-merit_img02@2x.png" alt="講師イラスト">
                 <div class="p-top__meritContentsSectionLeftDescription">
                     <span class="p-top__meritContentsSectionLeftDescriptionNumber">02</span>
                     <h2 class="c-textRed c-mb20 c-fs20 c-fwBold">講師はプロのエンジニア</h2>
@@ -22,7 +22,7 @@
             </div>
 
             <div class="p-top__meritContentsSectionRight">
-                <img class="p-top__meritContentsSectionRightImage" src="~/assets/images/p-merit_img03@2x.png" alt="スクールイラスト">
+                <img class="p-top__meritContentsSectionRightImage" src="~/assets/images/p-merit_img03@2x.png" alt="会話イラスト">
                 <div class="p-top__meritContentsSectionRightDescription">
                     <span class="p-top__meritContentsSectionRightDescriptionNumber">03</span>
                     <h2 class="c-textRed c-mb20 c-fs20 c-fwBold">個別のカリキュラム</h2>
@@ -31,7 +31,7 @@
             </div>
 
             <div class="p-top__meritContentsSectionLeft">
-                <img class="p-top__meritContentsSectionLeftImage" src="~/assets/images/p-merit_img04@2x.png" alt="スクールイラスト">
+                <img class="p-top__meritContentsSectionLeftImage" src="~/assets/images/p-merit_img04@2x.png" alt="作業イラスト">
                 <div class="p-top__meritContentsSectionLeftDescription">
                     <span class="p-top__meritContentsSectionLeftDescriptionNumber">04</span>
                     <h2 class="c-textRed c-mb20 c-fs20 c-fwBold">アウトプット中心</h2>
@@ -59,170 +59,90 @@ export default {
         background:linear-gradient(90deg,#E15F52 0%,#E15F52 20%,#FFEDE6 20%,#FFEDE6 100%);
         text-align: center;
         &Contents {
-        h1 {
-            padding-top: 40px;
-            margin: 0;
-        }
-        &Section {
-            &Right {
-                width: 330px;
-                margin: 0 0 0 auto;
-                text-align: left;
-                position: relative;
-                &Image {
-                    position: relative;
-                    top: 45px;
-                    width: 140px;
-                    height: 150px;
-                    z-index: 1;
-                }
-                &Description {
-                    background-color: #fff;
-                    width: 280px;
+            h1 {
+                padding-top: 40px;
+                margin: 0;
+            }
+            &Section {
+                &Right {
+                    width: 330px;
                     margin: 0 0 0 auto;
-                    padding: 0 20px 30px;
-                    text-align: center;
+                    text-align: left;
                     position: relative;
-                    &Number {
+                    &Image {
                         position: relative;
-                        top: -45px;
-                        font-size: 60px;
-                        color: #E15F52;
-                        opacity:0.3;
+                        top: 45px;
+                        width: 140px;
+                        height: 150px;
+                        z-index: 1;
                     }
-                    p {
-                        text-align: left;
-                        color: #333333;
-                        letter-spacing: 0.2em;
+                    &Description {
+                        background-color: #fff;
+                        width: 280px;
+                        margin: 0 0 0 auto;
+                        padding: 0 20px 30px;
+                        text-align: center;
+                        position: relative;
+                        &Number {
+                            position: relative;
+                            top: -45px;
+                            font-size: 60px;
+                            color: #E15F52;
+                            opacity:0.3;
+                        }
+                        p {
+                            text-align: left;
+                            color: #333333;
+                            letter-spacing: 0.2em;
+                        }
                     }
                 }
-            }
-            &Left {
-                width: 330px;
-                margin: 0 auto 0 0;
-                text-align: right;
-                position: relative;
-                &Image {
-                    position: relative;
-                    top: 45px;
-                    width: 140px;
-                    height: 150px;
-                    z-index: 1;
-                }
-                &Description {
-                    background-color: #fff;
-                    width: 280px;
+                &Left {
+                    width: 330px;
                     margin: 0 auto 0 0;
-                    padding: 0 20px 30px;
-                    text-align: center;
+                    text-align: right;
                     position: relative;
-                    &Number {
+                    &Image {
                         position: relative;
-                        top: -45px;
-                        font-size: 60px;
-                        color: #E15F52;
-                        opacity:0.3;
+                        top: 45px;
+                        width: 140px;
+                        height: 150px;
+                        z-index: 1;
                     }
-                    p {
-                        text-align: left;
-                        color: #333333;
-                        letter-spacing: 0.2em;
+                    &Description {
+                        background-color: #fff;
+                        width: 280px;
+                        margin: 0 auto 0 0;
+                        padding: 0 20px 30px;
+                        text-align: center;
+                        position: relative;
+                        &Number {
+                            position: relative;
+                            top: -45px;
+                            font-size: 60px;
+                            color: #E15F52;
+                            opacity:0.3;
+                        }
+                        p {
+                            text-align: left;
+                            color: #333333;
+                            letter-spacing: 0.2em;
+                        }
                     }
                 }
             }
-        }
-        &Document {
-            padding: 80px 0;
-            h3 {
-                letter-spacing: 0.1em;
-                line-height: 1.5em;
+            &Document {
+                padding: 80px 0;
+                h3 {
+                    letter-spacing: 0.1em;
+                    line-height: 1.5em;
+                }
+                a {
+                    width: 160px;
+                    height: 50px;
+                    margin: 0 auto;
+                }
             }
-            a {
-                width: 160px;
-                height: 50px;
-                margin: 0 auto;
-            }
         }
-    }
-
-    }
-    // .p-top_merit__contents {
-    //     h1 {
-    //         padding-top: 40px;
-    //         margin: 0;
-    //     }
-    //     &__section {
-    //         &--right {
-    //             width: 330px;
-    //             margin: 0 0 0 auto;
-    //             text-align: left;
-    //             position: relative;
-    //             &__description {
-    //                 background-color: #fff;
-    //                 width: 280px;
-    //                 margin: 0 0 0 auto;
-    //                 padding: 0 20px 30px;
-    //                 text-align: center;
-    //                 position: relative;
-    //                 &__number {
-    //                     position: relative;
-    //                     top: -45px;
-    //                     font-size: 60px;
-    //                     color: #E15F52;
-    //                     opacity:0.3;
-    //                 }
-    //                 p {
-    //                     text-align: left;
-    //                     color: #333333;
-    //                     letter-spacing: 0.2em;
-    //                 }
-    //             }
-    //         }
-    //         &--left {
-    //             width: 330px;
-    //             margin: 0 auto 0 0;
-    //             text-align: right;
-    //             position: relative;
-    //             &__description {
-    //                 background-color: #fff;
-    //                 width: 280px;
-    //                 margin: 0 auto 0 0;
-    //                 padding: 0 20px 30px;
-    //                 text-align: center;
-    //                 position: relative;
-    //                 &__number {
-    //                     position: relative;
-    //                     top: -45px;
-    //                     font-size: 60px;
-    //                     color: #E15F52;
-    //                     opacity:0.3;
-    //                 }
-    //                 p {
-    //                     text-align: left;
-    //                     color: #333333;
-    //                     letter-spacing: 0.2em;
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     &__document {
-    //         padding: 80px 0;
-    //         h3 {
-    //             letter-spacing: 0.1em;
-    //             line-height: 1.5em;
-    //         }
-    //         a {
-    //             width: 160px;
-    //             height: 50px;
-    //             margin: 0 auto;
-    //         }
-    //     }
-    // }
-    .p-top__merit__image {
-        position: relative;
-        top: 45px;
-        width: 140px;
-        height: 150px;
-        z-index: 1;
     }
 </style>
