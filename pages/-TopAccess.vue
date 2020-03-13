c8cdd34ee183b3bfd89e51a5a4ea33ff87a95be8
中村駿輝 <shnklife@gmail.com>
2020-03-13 12:56:30 +0900

    コーディング

--- pages/-TopAccess.vue
+++ pages/-TopAccess.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="p-top__access">
-        <h1 class="p-top__titles c-textRed">Access</h1>
-        <img width="335px" height="235px" src="~/assets/images/top_img1.jpg" alt="">
-        <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+    <div class="p-access">
+        <h1 class="p-access__title">Access</h1>
+        <div class="p-access__map"></div>
+        <p class="p-access__address">
+            530-0041<br>大阪府大阪市北区天満2丁目8-17<br>天満橋パークビル 501
+        </p>
+        <div class="p-access__btn">
+            <router-link class="c-buttonWhite c-fs20 c-fwBold" to="">Google Map</router-link>
+        </div>
     </div>
 </template>
 
@@ -13,5 +18,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    .p-access {
+        margin: 80px 0;
+        &__title {
+            text-align: center;
+            margin: 0 auto 20px;
+            font-size: 40px;
+            letter-spacing: 0.2em;
+            color: #E15F52;
+        }
+        &__map {
+            width: 335px;
+            height: 236px;
+            background-color: gray;
+            margin: 0 auto 20px;
+        }
+        &__address {
+            width: 335px;
+            margin: 0 auto 20px;
+            font-size: 14px;
+        }
+        &__btn {
+            width: 160px;
+            height: 50px;
+            margin: 0 auto;
+            line-height: 50px;
+        }
+    }
 </style>
