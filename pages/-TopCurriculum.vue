7a8f907b0b945b98394b18e2dabdbc27d7065e85
shunking-tech <52094419+shunking-tech@users.noreply.github.com>
2020-03-13 16:46:05 +0900

    カリキュラム(TopCurriculum.vue)のコーディング (#18)
    * -TopCurriculum.vue修正
    
    * -TopReason.vue修正
    
    * scssの入れ子修正
    
    * scssの入れ子修正
    
    Co-authored-by: 中村駿輝 <shnklife@gmail.com>

--- pages/-TopCurriculum.vue
+++ pages/-TopCurriculum.vue
@@ -1,68 +1,68 @@
 <template>
-    <div class="p-top__curriculum">
+    <div class="p-curriculum">
         <h1 class="p-top__titles c-textRed">Curriculum</h1>
 
-        <div class="p-top__curriculum__section">
-            <div class="p-top__curriculum__section__title c-mb20">
+        <div class="p-curriculum__section">
+            <div class="p-curriculum__sectionTitle">
                 <h3 class="c-textRed c-fs14 c-fwBold">基礎からしっかり学びたいあなたに</h3>
                 <h2 class="c-fs24 c-fwBold">初心者向けコース</h2>
             </div>
-            <div class="p-top__curriculum__section__content">
-                <img class="p-top__curriculum__image" src="~/assets/images/p-curriculum_img01@2x.png" alt="ウィンドウのイラスト">
-                <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
-                <ul class="p-top__curriculum__section__content__categories d-flex">
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+            <div class="p-curriculum__sectionContent">
+                <img class="p-curriculum__sectionContentImage" src="~/assets/images/p-curriculum_img01@2x.png" alt="ウィンドウのイラスト">
+                <p class="p-curriculum__sectionContentDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                <ul class="p-curriculum__categories">
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
                 </ul>
             </div>
         </div>
 
-        <div class="p-top__curriculum__section">
-            <div class="p-top__curriculum__section__title c-mb20">
-                <h3 class="c-textRed c-fs14 c-fwBold">仕事で活用できるスキルを</h3>
-                <h2 class="c-fs24 c-fwBold">FRONTコース</h2>
+        <div class="p-curriculum__section">
+            <div class="p-curriculum__sectionTitle">
+                <h3 class="c-textRed c-fs14 c-fwBold">基礎からしっかり学びたいあなたに</h3>
+                <h2 class="c-fs24 c-fwBold">初心者向けコース</h2>
             </div>
-            <div class="p-top__curriculum__section__content">
-                <img class="p-top__curriculum__image" src="~/assets/images/p-curriculum_img02@2x.png" alt="ウィンドウのイラスト">
-                <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
-                <ul class="p-top__curriculum__section__content__categories d-flex">
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+            <div class="p-curriculum__sectionContent">
+                <img class="p-curriculum__sectionContentImage" src="~/assets/images/p-curriculum_img01@2x.png" alt="ウィンドウのイラスト">
+                <p class="p-curriculum__sectionContentDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                <ul class="p-curriculum__categories">
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
                 </ul>
             </div>
         </div>
 
-        <div class="p-top__curriculum__section">
-            <div class="p-top__curriculum__section__title c-mb20">
+        <div class="p-curriculum__section">
+            <div class="p-curriculum__sectionTitle">
                 <h3 class="c-textRed c-fs14 c-fwBold">基礎からしっかり学びたいあなたに</h3>
                 <h2 class="c-fs24 c-fwBold">初心者向けコース</h2>
             </div>
-            <div class="p-top__curriculum__section__content">
-                <img class="p-top__curriculum__image" src="~/assets/images/p-curriculum_img01@2x.png" alt="ウィンドウのイラスト">
-                <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
-                <ul class="p-top__curriculum__section__content__categories d-flex">
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+            <div class="p-curriculum__sectionContent">
+                <img class="p-curriculum__sectionContentImage" src="~/assets/images/p-curriculum_img01@2x.png" alt="ウィンドウのイラスト">
+                <p class="p-curriculum__sectionContentDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                <ul class="p-curriculum__categories">
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
-                    <li class="p-top__curriculum__section__content__categories__item">
-                        <router-link class="c-fs14" to="">HTML</router-link>
+                    <li class="p-curriculum__categoriesItem">
+                        <router-link class="p-curriculum__categoriesItemText" to="">HTML</router-link>
                     </li>
                 </ul>
             </div>
@@ -77,7 +77,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .p-top__curriculum {
+    .p-curriculum {
         text-align: center;
         &__section {
             width: 330px;
@@ -85,47 +85,55 @@ export default {
             margin: 0 auto;
             margin-bottom: 55px;
             background-color: #FFEDE6;
-            &__title {
-                text-align: left;
-            }
-            &__content {
-                img {
-                    width: 100px;
-                    height: 90px;
-                    margin-bottom: 10px;
-                }
-                p {
-                    text-align: left;
-                }
-                &__categories {
-                    padding: 0;
-                    &__item {
-                        list-style: none;
-                        margin-right: 5px;
-                        background-color: #E15F52;
-                        padding: 5px 8px;
-                        border-radius: 5px;
-                        a {
-                            color: #fff;
-                            letter-spacing: 0.1em;
-                        }
-                    }
-                }
-            }
-            &:nth-child(odd) {
-                background-color: #fff;
-                border: 2px solid #FFEDE6;
-            }
+            position: relative;
         }
-    }
-    .p-top_curriculum__title-front {
-        text-align: right;
-    }
-    .p-top_curriculum__image {
-        width: 100px;
-        height: 100px;
-    }
-    .p-top_curriculumHr {
-        margin: 20px auto;
+        &__section::after {
+            content: '';
+            display: block;
+            padding: 70% 50%;
+            border: 2px solid #FFEDE6;
+            position: absolute;
+            bottom: -3%;
+            right: -5%;
+            z-index: -1;
+        }
+        &__section:nth-child(odd) {
+            background-color: #fff;
+            border: 2px solid #FFEDE6;
+        }
+        &__section:nth-child(odd)::after {
+            background-color: #FFEDE6;
+            left: -5%;
+            right: 5%;
+            padding: 70% 50%;
+        }
+        &__sectionTitle {
+            text-align: left;
+            margin-bottom: 20px;
+        }
+        &__sectionContentImage {
+            width: 100px;
+            height: 90px;
+            margin-bottom: 10px;
+        }
+        &__sectionContentDescription {
+            text-align: left;
+        }
+        &__categories {
+            padding: 0;
+            display: flex;
+        }
+        &__categoriesItem {
+            list-style: none;
+            margin-right: 5px;
+            background-color: #E15F52;
+            padding: 5px 8px;
+            border-radius: 5px;
+        }
+        &__categoriesItemText {
+            color: #fff;
+            letter-spacing: 0.1em;
+            font-size: 14px;
+        }
     }
 </style>
