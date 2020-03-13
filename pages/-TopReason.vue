6b553eb35b117985e494ab5d1d9dd09d526a7cfd
shunking-tech <52094419+shunking-tech@users.noreply.github.com>
2020-03-13 16:46:17 +0900

    Reason(TopReason.vue)のコーディング (#20)
    * first connit
    
    * -TopReason.vue修正
    
    * scssの入れ子修正
    
    * scssの入れ子修正
    
    Co-authored-by: 中村駿輝 <shnklife@gmail.com>

--- pages/-TopReason.vue
+++ pages/-TopReason.vue
@@ -1,31 +1,38 @@
 <template>
-    <div class="p-top__reason">
+    <div class="p-reason">
         <h1 class="p-top__titles c-textRed">Reason</h1>
-        <div class="p-top_reason__point01">
-            <img class="p-top_reason__image" src="~/assets/images/top_img1.jpg" alt="">
-            <div class="p-top_reason__box">
-                <h3>現場で使える技術を</h3>
+        <div class="p-reason__point">
+            <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading01@2x.png" alt="手のイラスト">
+            <div class="p-reason__pointBox">
+                <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img01@2x.png" alt="">
+                <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
+                <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+            </div>
+        </div>
+
+        <div class="p-reason__point">
+            <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading02@2x.png" alt="ハートのイラスト">
+            <div class="p-reason__pointBox">
+                <img src="~/assets/images/p-reason_img02@2x.png" alt="">
+                <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
                 <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
             </div>
         </div>
-        <div class="p-top_reason__point02">
-            <img class="p-top_reason__image" src="~/assets/images/top_img1.jpg" alt="">
-            <div class="p-top_reason__box">
-                <h3>個別カリキュラム</h3>
+
+        <div class="p-reason__point">
+            <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading03@2x.png" alt="PCのイラスト">
+            <div class="p-reason__pointBox">
+                <img src="~/assets/images/p-reason_img03@2x.png" alt="">
+                <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
                 <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
             </div>
         </div>
-        <div class="p-top_reason__point03">
-            <img class="p-top_reason__image" src="~/assets/images/top_img1.jpg" alt="">
-            <div class="p-top_reason__box">
-                <h3>自分で学べる学習スタイル</h3>
-                <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
-            </div>
-        </div>
-        <div class="p-top_reason__point04">
-            <img class="p-top_reason__image" src="~/assets/images/top_img1.jpg" alt="">
-            <div class="p-top_reason__box">
-                <h3>縛りなしで１ヶ月ごとの契約</h3>
+
+        <div class="p-reason__point">
+            <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading04@2x.png" alt="カレンダーのイラスト">
+            <div class="p-reason__pointBox">
+                <img src="~/assets/images/p-reason_img04@2x.png" alt="">
+                <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
                 <p>説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
             </div>
         </div>
@@ -39,16 +46,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .p-top__reason {
+    .p-reason {
         background-color: #FFEDE6;
-        text-align: center;
-    }
-    .p-top_reason__image {
-        width: 120px;
-        height: 120px;
-    }
-    .p-top_reason__box {
-        background-color: #ffffff;
-        border-radius: 10px;
+        text-align: left;
+        padding: 0 20px;
+        &__point {
+            padding-bottom: 40px;
+        }
+        &__pointImage {
+            width: 72px;
+            height: 72px;
+            margin-left: 10px;
+        }
+        &__pointBox {
+            background-color: #ffffff;
+            padding: 0 42px 30px;
+            text-align: center;
+            position: relative;
+        }
+        &__pointBoxImage {
+            display: inline-block;
+            width: 80px;
+            height: 80px;
+            position: relative;
+            top: -50px;
+        }
+        &__pointBoxDescription {
+            text-align: left;
+            line-height: 2em;
+            letter-spacing: 0.1em;
+        }
     }
 </style>
