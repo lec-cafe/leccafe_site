51bd1d9adfde2dd07946622d3ed2ac410c82869e
shunking-tech <52094419+shunking-tech@users.noreply.github.com>
2020-03-13 16:47:05 +0900

    Meritのクラス名とSCSS入れ子状態の修正 (#28)
    * first commit
    
    * クラス名とSCSS入れ子状態の修正
    
    Co-authored-by: 中村駿輝 <shnklife@gmail.com>

--- pages/-TopMerit.vue
+++ pages/-TopMerit.vue
@@ -1,45 +1,45 @@
 <template>
-    <div class="p-top__merit">
-        <div class="p-top__meritContents">
-            <h1 class="p-top__titles c-textRed">Merit</h1>
+    <div class="p-merit">
+        <div class="p-merit__contents">
+            <h1 class="p-merit__contentsTitle">Merit</h1>
 
-            <div class="p-top__meritContentsSectionRight">
-                <img class="p-top__meritContentsSectionRightImage" src="~/assets/images/p-merit_img01@2x.png" alt="スクールイラスト">
-                <div class="p-top__meritContentsSectionRightDescription">
-                    <span class="p-top__meritContentsSectionRightDescriptionNumber">01</span>
+            <div class="p-merit__contentsSection">
+                <img class="p-merit__contentsSectionImage" src="~/assets/images/p-merit_img01@2x.png" alt="スクールイラスト">
+                <div class="p-merit__contentsSectionDescription">
+                    <span class="p-merit__contentsSectionDescriptionNumber">01</span>
                     <h2 class="c-textRed c-mb20 c-fs20 c-fwBold">制作会社運営スクール</h2>
-                    <p class="c-textRed">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                    <p class="p-merit__contentsSectionDescriptionText">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
                 </div>
             </div>
 
-            <div class="p-top__meritContentsSectionLeft">
-                <img class="p-top__meritContentsSectionLeftImage" src="~/assets/images/p-merit_img02@2x.png" alt="講師イラスト">
-                <div class="p-top__meritContentsSectionLeftDescription">
-                    <span class="p-top__meritContentsSectionLeftDescriptionNumber">02</span>
+            <div class="p-merit__contentsSection">
+                <img class="p-merit__contentsSectionImage" src="~/assets/images/p-merit_img02@2x.png" alt="講師イラスト">
+                <div class="p-merit__contentsSectionDescription">
+                    <span class="p-merit__contentsSectionDescriptionNumber">02</span>
                     <h2 class="c-textRed c-mb20 c-fs20 c-fwBold">講師はプロのエンジニア</h2>
-                    <p class="c-textRed">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                    <p class="p-merit__contentsSectionDescriptionText">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
                 </div>
             </div>
 
-            <div class="p-top__meritContentsSectionRight">
-                <img class="p-top__meritContentsSectionRightImage" src="~/assets/images/p-merit_img03@2x.png" alt="会話イラスト">
-                <div class="p-top__meritContentsSectionRightDescription">
-                    <span class="p-top__meritContentsSectionRightDescriptionNumber">03</span>
+            <div class="p-merit__contentsSection">
+                <img class="p-merit__contentsSectionImage" src="~/assets/images/p-merit_img03@2x.png" alt="会話イラスト">
+                <div class="p-merit__contentsSectionDescription">
+                    <span class="p-merit__contentsSectionDescriptionNumber">03</span>
                     <h2 class="c-textRed c-mb20 c-fs20 c-fwBold">個別のカリキュラム</h2>
-                    <p class="c-textRed">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                    <p class="p-merit__contentsSectionDescriptionText">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
                 </div>
             </div>
 
-            <div class="p-top__meritContentsSectionLeft">
-                <img class="p-top__meritContentsSectionLeftImage" src="~/assets/images/p-merit_img04@2x.png" alt="作業イラスト">
-                <div class="p-top__meritContentsSectionLeftDescription">
-                    <span class="p-top__meritContentsSectionLeftDescriptionNumber">04</span>
+            <div class="p-merit__contentsSection">
+                <img class="p-merit__contentsSectionImage" src="~/assets/images/p-merit_img04@2x.png" alt="作業イラスト">
+                <div class="p-merit__contentsSectionDescription">
+                    <span class="p-merit__contentsSectionDescriptionNumber">04</span>
                     <h2 class="c-textRed c-mb20 c-fs20 c-fwBold">アウトプット中心</h2>
-                    <p class="c-textRed">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                    <p class="p-merit__contentsSectionDescriptionText">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
                 </div>
             </div>
 
-            <div class="p-top__meritContentsDocument">
+            <div class="p-merit__contentsDocument">
                 <h3 class="c-fs14 c-fwBold c-textRed c-mb20">詳しく知りたい方は<br>まずは資料請求！</h3>
                 <router-link class="c-buttonWhite c-fs20 c-fwBold" to="">資料請求</router-link>
             </div>
@@ -55,94 +55,69 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .p-top__merit {
+    .p-merit {
         background:linear-gradient(90deg,#E15F52 0%,#E15F52 20%,#FFEDE6 20%,#FFEDE6 100%);
         text-align: center;
-        &Contents {
-            h1 {
-                padding-top: 40px;
-                margin: 0;
-            }
-            &Section {
-                &Right {
-                    width: 330px;
-                    margin: 0 0 0 auto;
-                    text-align: left;
-                    position: relative;
-                    &Image {
-                        position: relative;
-                        top: 45px;
-                        width: 140px;
-                        height: 150px;
-                        z-index: 1;
-                    }
-                    &Description {
-                        background-color: #fff;
-                        width: 280px;
-                        margin: 0 0 0 auto;
-                        padding: 0 20px 30px;
-                        text-align: center;
-                        position: relative;
-                        &Number {
-                            position: relative;
-                            top: -45px;
-                            font-size: 60px;
-                            color: #E15F52;
-                            opacity:0.3;
-                        }
-                        p {
-                            text-align: left;
-                            color: #333333;
-                            letter-spacing: 0.2em;
-                        }
-                    }
-                }
-                &Left {
-                    width: 330px;
-                    margin: 0 auto 0 0;
-                    text-align: right;
-                    position: relative;
-                    &Image {
-                        position: relative;
-                        top: 45px;
-                        width: 140px;
-                        height: 150px;
-                        z-index: 1;
-                    }
-                    &Description {
-                        background-color: #fff;
-                        width: 280px;
-                        margin: 0 auto 0 0;
-                        padding: 0 20px 30px;
-                        text-align: center;
-                        position: relative;
-                        &Number {
-                            position: relative;
-                            top: -45px;
-                            font-size: 60px;
-                            color: #E15F52;
-                            opacity:0.3;
-                        }
-                        p {
-                            text-align: left;
-                            color: #333333;
-                            letter-spacing: 0.2em;
-                        }
-                    }
-                }
-            }
-            &Document {
-                padding: 80px 0;
-                h3 {
-                    letter-spacing: 0.1em;
-                    line-height: 1.5em;
-                }
-                a {
-                    width: 160px;
-                    height: 50px;
-                    margin: 0 auto;
-                }
-            }
+        &__contentsTitle {
+            text-align: center;
+            margin: 40px auto 0;
+            padding-top: 40px;
+            font-size: 40px;
+            letter-spacing: 0.2em;
+            color: #E15F52;
+        }
+        &__contentsSection {
+            width: 330px;
+            margin: 0 0 0 auto;
+            text-align: left;
+            position: relative;
+        }
+        &__contentsSectionImage {
+            position: relative;
+            top: 45px;
+            width: 140px;
+            height: 150px;
+            z-index: 1;
+        }
+        &__contentsSectionDescription {
+            background-color: #fff;
+            width: 280px;
+            margin: 0 0 0 auto;
+            padding: 0 20px 30px;
+            text-align: center;
+            position: relative;
+        }
+        &__contentsSectionDescriptionNumber {
+            position: relative;
+            top: -45px;
+            font-size: 60px;
+            color: #E15F52;
+            opacity:0.3;
+        }
+        &__contentsSectionDescriptionText {
+                text-align: left;
+                color: #333333;
+                letter-spacing: 0.2em;
+        }
+
+        &__contentsSection:nth-child(odd) {
+            margin: 0 auto 0 0;
+            text-align: right;
+        }
+        &__contentsSection:nth-child(odd) &__contentsSectionDescription {
+            margin: 0 auto 0 0;
+        }
+        &__contentsDocument {
+            padding: 80px 0;
+        }
+        &__contentsDocument h3 {
+            letter-spacing: 0.1em;
+            line-height: 1.5em;
+        }
+        &__contentsDocument a {
+            width: 160px;
+            height: 50px;
+            margin: 0 auto;
         }
     }
 </style>
