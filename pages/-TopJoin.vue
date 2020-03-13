1f966d8346bdb846ec1ee2f324831ac4cea29d85
kogachan4 <kogachan4@gmail.com>
2020-03-13 13:50:31 +0900

    Join(TopJoin.vue)

--- pages/-TopJoin.vue
+++ pages/-TopJoin.vue
@@ -3,8 +3,12 @@
         <h1>Join</h1>
         <div class="p-top_join__box">
             <h3>詳しく知りたい方は<br>まずは資料請求！</h3>
-            <button>資料請求</button>
-            <button>申し込み</button>
+            <div class="p-claim">
+                <a>資料請求</a>  
+            </div>
+            <div class="p-apply">  
+                <a>申し込み</a>
+            </div>
         </div>
     </div>
 </template>
@@ -19,9 +23,55 @@ export default {
     .p-top__join {
         background-color: #E15F52;
         text-align: center;
+        height: 560px;
+        color: #FFFFFF;
+    }
+    .p-top__join h1 {
+        padding-top: 50px;
+        font-family: Voces,Voces;
+        font-size: 40px;
     }
     .p-top_join__box {
-        border: solid 3px #ffffff;
-        border-radius: 10px;
+        color: #FFFFFF;
+        border: solid 4px #ffffff;
+        // border-radius: 10px;
+        margin-top: 50px;
+        height: 346px;
+        margin-right: 20px;
+        margin-left: 20px;
+    }
+    .p-top_join__box h3 {
+        font-size: 18px;
+        margin-top: 60px;
+        font-weight: bold;
+        margin-bottom: 50px;
+        // font-family: 游ゴシック体;
+    }
+    .p-claim {
+        display: block;
+        margin: 0 auto;
+        width: 160px;
+        height: 50px;
+        color:#E15F52;
+        background-color: #FFFFFF;
+        padding-top: 8px;
+        border: solid 2px;
+        border-radius: 25px;
+        font-size: 22px;
+        font-weight: bold;
+    }
+    .p-apply {
+        background-color: #E15F52;
+        display: block;
+        margin: 0 auto;
+        width: 160px;
+        height: 50px;
+        color: #FFFFFF;
+        padding-top: 8px;
+        border: solid 2px;
+        margin-top: 30px;
+        border-radius: 25px;
+        font-size: 22px;
+        font-weight: bold;
     }
 </style>
