7250e4edc8650f67d6c7adb64f349ad722f14aad
Akiyoshi Koga <55970295+kogachan4@users.noreply.github.com>
2020-03-13 16:46:42 +0900

    Join(TopJoin.vue) (#23)
    * Join(TopJoin.vue)
    
    * Join(TopJoin.vue)
    
    Co-authored-by: kogachan4 <kogachan4@gmail.com>

--- pages/-TopJoin.vue
+++ pages/-TopJoin.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="p-top__join">
+    <div class="p-join">
         <h1>Join</h1>
-        <div class="p-top_join__box">
+        <div class="p-join__box">
             <h3>詳しく知りたい方は<br>まずは資料請求！</h3>
-            <button>資料請求</button>
-            <button>申し込み</button>
+            <div class="p-claim">
+                <a>資料請求</a>  
+            </div>
+            <div class="p-apply">  
+                <a>申し込み</a>
+            </div>
         </div>
     </div>
 </template>
@@ -16,12 +20,56 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .p-top__join {
+    .p-join {
         background-color: #E15F52;
         text-align: center;
+        height: 560px;
+        color: #FFFFFF;
     }
-    .p-top_join__box {
-        border: solid 3px #ffffff;
-        border-radius: 10px;
+    .p-join h1 {
+        padding-top: 50px;
+        font-family: Voces,Voces;
+        font-size: 40px;
+    }
+    .p-join__box {
+        color: #FFFFFF;
+        border: solid 4px #ffffff;
+        margin-top: 50px;
+        height: 346px;
+        margin-right: 20px;
+        margin-left: 20px;
+    }
+    .p-join__box h3 {
+        font-size: 18px;
+        margin-top: 60px;
+        font-weight: bold;
+        margin-bottom: 50px;
+    }
+    .p-claim {
+        display: block;
+        margin: 0 auto;
+        width: 160px;
+        height: 50px;
+        color:#E15F52;
+        background-color: #FFFFFF;
+        padding-top: 8px;
+        border: solid 2px;
+        border-radius: 25px;
+        font-size: 22px;
+        font-weight: bold;
+    }
+    .p-apply {
+        background-color: #E15F52;
+        display: block;
+        margin: 0 auto;
+        width: 160px;
+        height: 50px;
+        color: #FFFFFF;
+        padding-top: 8px;
+        border: solid 2px;
+        margin-top: 30px;
+        border-radius: 25px;
+        font-size: 22px;
+        font-weight: bold;
     }
 </style>
