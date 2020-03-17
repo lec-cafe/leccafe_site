f530664714e8bfe319832d5bc62923b83e0e1a3c
shunking-tech <52094419+shunking-tech@users.noreply.github.com>
2020-03-17 14:06:33 +0900

    余白調整 (#60)
    Co-authored-by: 中村駿輝 <shnklife@gmail.com>

--- pages/-TopReason.vue
+++ pages/-TopReason.vue
@@ -1,40 +1,42 @@
 <template>
     <div class="p-reason">
-        <h1 class="c-titles c-textRed">Reason</h1>
-        <div class="flex">
-            <div class="p-reason__point 1">
-                <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading01@2x.png" alt="手のイラスト">
-                <div class="p-reason__pointBox">
-                    <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img01@2x.png" alt="">
-                    <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
-                    <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+        <div class="p-reason__container">
+            <h1 class="c-titles c-textRed">Reason</h1>
+            <div class="flex">
+                <div class="p-reason__point 1">
+                    <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading01@2x.png" alt="手のイラスト">
+                    <div class="p-reason__pointBox">
+                        <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img01@2x.png" alt="">
+                        <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
+                        <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                    </div>
                 </div>
-            </div>
-            <div class="p-reason__point">
-                <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading02@2x.png" alt="ハートのイラスト">
-                <div class="p-reason__pointBox">
-                    <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img02@2x.png" alt="">
-                    <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
-                    <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                <div class="p-reason__point">
+                    <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading02@2x.png" alt="ハートのイラスト">
+                    <div class="p-reason__pointBox">
+                        <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img02@2x.png" alt="">
+                        <h3 class="c-fs20 c-textRed c-fwBold">個別カリキュラム</h3>
+                        <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                    </div>
                 </div>
-            </div>
-        </div>   
-        <div class="flex"> 
-            <div class="p-reason__point 1">
-                <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading03@2x.png" alt="PCのイラスト">
-                <div class="p-reason__pointBox">
-                    <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img03@2x.png" alt="">
-                    <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
-                    <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+            </div>   
+            <div class="flex"> 
+                <div class="p-reason__point 1">
+                    <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading03@2x.png" alt="PCのイラスト">
+                    <div class="p-reason__pointBox">
+                        <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img03@2x.png" alt="">
+                        <h3 class="c-fs20 c-textRed c-fwBold">自分で学べる学習スタイル</h3>
+                        <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                    </div>
                 </div>
-            </div>
 
-            <div class="p-reason__point">
-                <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading04@2x.png" alt="カレンダーのイラスト">
-                <div class="p-reason__pointBox">
-                    <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img04@2x.png" alt="">
-                    <h3 class="c-fs20 c-textRed c-fwBold">現場で使える技術を</h3>
-                    <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                <div class="p-reason__point">
+                    <img class="p-reason__pointImage" src="~/assets/images/p-reason_heading04@2x.png" alt="カレンダーのイラスト">
+                    <div class="p-reason__pointBox">
+                        <img class="p-reason__pointBoxImage" src="~/assets/images/p-reason_img04@2x.png" alt="">
+                        <h3 class="c-fs20 c-textRed c-fwBold">縛りなしで１ヶ月ごとの契約</h3>
+                        <p class="p-reason__pointBoxDescription">説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ説明文が入るよ</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -48,10 +50,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~/assets/scss/mixins/container.scss";
+
     .p-reason {
         background-color: #FFEDE6;
         text-align: left;
-        padding: 0 20px;
+        padding: 0 20px 30px;
+        &__container {
+            @include container();
+            padding-bottom: 0;
+        }
         &__point {
             padding-bottom: 40px;
         }
@@ -62,7 +70,6 @@ export default {
         }
         &__pointBox {
             background-color: #ffffff;
-            padding: 0 42px 30px;
             text-align: center;
             position: relative;
         }
@@ -77,24 +84,32 @@ export default {
             text-align: left;
             line-height: 2em;
             letter-spacing: 0.1em;
+            font-size: 14px;
+            padding: 0 42px;
+        }
+        &__pointBox h3, &__pointBox p {
+            position: relative;
+            top: -30px;
         }
     }
     @media screen and (min-width: 780px) {
         .flex {
             display: flex;
-            justify-content: center;
+            justify-content: space-between;
         }
-        .p-reason__point {
-            height: auto;
-            margin-top: 0px;
+        .p-reason {
+            padding-bottom: 50px;
+            &__point {
+                height: auto;
+                margin-top: 0px;
+            }
+            &__pointBox {
+                width: 335px;
+            }
+            &__pointImage {
+                margin-left: 10px;
+                margin-bottom: 20px;
+            }
         }
-        .p-reason__pointBox {
-            width: 380px;
-            margin-left: 80px;
-        }
-        .p-reason__pointImage {
-            margin-left: 80px;
-            margin-bottom: 20px;
-        }    
     }
 </style>
