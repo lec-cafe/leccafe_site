3c0a7c560aa12f823471da69d9db432af94b569d
中村駿輝 <shnklife@gmail.com>
2020-03-26 14:10:52 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,12 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
 
       //OGP
-      { hid: 'description', name: 'description', content: '共通ディスクリプション' },
-      { hid: 'og:site_name', property: 'og:site_name', content: 'サイト名' },
+      { hid: 'description', name: 'description', content: '手軽に始めるプログラミングスクール' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Lec Cafe' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: 'https://example.com' },
-      { hid: 'og:title', property: 'og:title', content: 'サイト名' },
-      { hid: 'og:description', property: 'og:description', content: '共通ディスクリプション' },
+      { hid: 'og:title', property: 'og:title', content: 'Lec Cafe' },
+      { hid: 'og:description', property: 'og:description', content: '手軽に始めるプログラミングスクール' },
       { hid: 'og:image', property: 'og:image', content: `/static/OGP.png` },
-      { name: 'twitter:card', content: 'summary_large_image' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
