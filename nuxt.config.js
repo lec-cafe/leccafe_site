eb5bd1bd4f1376f775367197001ff502f406b081
中村駿輝 <shnklife@gmail.com>
2020-03-26 14:01:27 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      //OGP
+      { hid: 'description', name: 'description', content: '共通ディスクリプション' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'サイト名' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://example.com' },
+      { hid: 'og:title', property: 'og:title', content: 'サイト名' },
+      { hid: 'og:description', property: 'og:description', content: '共通ディスクリプション' },
       { hid: 'og:image', property: 'og:image', content: `/static/OGP.png` },
       { name: 'twitter:card', content: 'summary_large_image' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
